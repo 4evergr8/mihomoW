db89af4fbde60e9030d788a5d7d4146f1962ce8e
4evergr8 <137018415+4evergr8@users.noreply.github.com>
2026-06-13 23:37:53 +0800

    Update main.go

--- main.go
+++ main.go
@@ -3,17 +3,22 @@ package main
 import (
 	"fyne.io/fyne/v2"
 	"fyne.io/fyne/v2/app"
+	"fyne.io/fyne/v2/container"
 	"fyne.io/fyne/v2/widget"
 )
 
 func main() {
 	a := app.New()
-	w := a.NewWindow("demo")
+	w := a.NewWindow("Tab Demo")
 
-	w.SetContent(widget.NewButton("点击", func() {
-		w.SetTitle("clicked")
-	}))
+	tabs := container.NewAppTabs(
+		container.NewTabItem("节点", widget.NewLabel("节点页面")),
+		container.NewTabItem("规则", widget.NewLabel("规则页面")),
+		container.NewTabItem("设置", widget.NewLabel("设置页面")),
+	)
 
-	w.Resize(fyne.NewSize(300, 200))
+	w.SetContent(tabs)
+	w.Resize(fyne.NewSize(400, 300))
+
 	w.ShowAndRun()
 }
